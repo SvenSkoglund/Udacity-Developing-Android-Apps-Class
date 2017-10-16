cb803d619e924fecd1218bb2e601caa0473205f7
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-16 12:27:50 +0700

    :+1:

--- Projects/InternetToys/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Projects/InternetToys/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -20,6 +20,7 @@ import android.net.Uri;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -47,12 +48,16 @@ public class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        Uri builtUri = Uri.parse(GITHUB_BASE_URL);
-        builtUri.buildUpon(PARAM_QUERY,githubSearchQuery)
+        //  (1) Fill in this method to build the proper Github query URL
+        Uri builtUri = Uri.parse(GITHUB_BASE_URL).buildUpon().appendQueryParameter(PARAM_QUERY,githubSearchQuery).appendQueryParameter(PARAM_SORT,sortBy).build();
+        URL url = null;
+        try{
+            url = new URL(builtUri.toString());
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
 
-
-        return null;
+        return url;
     }
 
     /**
