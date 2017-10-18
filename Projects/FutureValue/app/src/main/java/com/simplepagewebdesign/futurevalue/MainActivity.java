3dd2441d567ecb382dfa65c61551b126e9702c65
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-10-18 13:01:47 -0600

    :100:

--- Projects/FutureValue/app/src/main/java/com/simplepagewebdesign/futurevalue/MainActivity.java
+++ Projects/FutureValue/app/src/main/java/com/simplepagewebdesign/futurevalue/MainActivity.java
@@ -2,25 +2,49 @@ package com.simplepagewebdesign.futurevalue;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText presentValue;
+    private TextView futureValueResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView presentValue;
-        TextView futureValue;
+        presentValue = (EditText) findViewById(R.id.presentValue);
+        futureValueResult = (TextView) findViewById(R.id.futureValueResult);
+        final Button calcButton = (Button) findViewById(R.id.calculateButton);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                returnValue(presentValue.getText().toString());
+                // Code here executes on main thread after user presses button
+            }
+        });
+        final Button clearButton = (Button) findViewById(R.id.clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearValues();
+                //Code here executes on main threa10d after user presses button
+            }
+        });
     }
-}
-    public void clearValues () {
 
+    public void clearValues() {
+        presentValue.setText("");
+        futureValueResult.setText("");
     }
 
-    public float returnValue (int input){
-        float output = input;
-        return output;
+    public double returnValue(String input) {
+        double inFloat = Double.parseDouble(input);
+        double rate = .1;
+        double years = 20;
+        double outFloat = inFloat * Math.pow((1f + rate), years);
+        futureValueResult.setText(String.valueOf(outFloat));
+        return outFloat;
     }
+
 }
