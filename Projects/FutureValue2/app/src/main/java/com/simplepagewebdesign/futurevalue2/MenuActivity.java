4f96801cf0b5ce03bf904121083909626bf73ce0
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-12-04 08:34:38 -0700

    :+1:

--- Projects/FutureValue2/app/src/main/java/com/simplepagewebdesign/futurevalue2/MenuActivity.java
+++ Projects/FutureValue2/app/src/main/java/com/simplepagewebdesign/futurevalue2/MenuActivity.java
@@ -2,12 +2,26 @@ package com.simplepagewebdesign.futurevalue2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuActivity extends AppCompatActivity {
 
+    public EditText interestRate;
+    public EditText years;
+    public Button submitButton;
+    public Button resetButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        interestRate = (EditText) findViewById(R.id.interestRate);
+        years = (EditText) findViewById(R.id.years);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        resetButton = (Button) findViewById(R.id.resetButton);
+
+
     }
 }
