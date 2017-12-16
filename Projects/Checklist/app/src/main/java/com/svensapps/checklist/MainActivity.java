20584210588bee7e332f9f7904c6dba0d4331fcf
SvenSkoglund <32058317+SvenSkoglund@users.noreply.github.com>
2017-12-16 15:47:23 -0700

    :+1:

--- Projects/Checklist/app/src/main/java/com/svensapps/checklist/MainActivity.java
+++ Projects/Checklist/app/src/main/java/com/svensapps/checklist/MainActivity.java
@@ -4,6 +4,7 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CheckBox chk1 = (CheckBox) findViewById(R.id.chk1);
+        chk1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (chk1.isChecked()) {
+                    onCheckboxClicked(chk1);
+                }else{
+                    onCheckboxUnClicked(chk1);
+                }
+            }
+        });
+        final CheckBox chk2 = (CheckBox) findViewById(R.id.chk2);
+        chk2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckboxClicked(chk2);
+            }
+        });
+        final CheckBox chk3 = (CheckBox) findViewById(R.id.chk3);
+        chk3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckboxClicked(chk3);
+            }
+        });
+        final CheckBox chk4 = (CheckBox) findViewById(R.id.chk4);
+        chk4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                onCheckboxClicked(chk4);
+            }
+        });
+        final CheckBox chk5 = (CheckBox) findViewById(R.id.chk5);
+        chk5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckboxClicked(chk5);
+            }
+        });
+        final CheckBox chk6 = (CheckBox) findViewById(R.id.chk6);
+        chk6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onCheckboxClicked(chk6);
+            }
+        });
+
     }
 
-    public void onCheckboxClicked(View view) {
-        final MediaPlayer cheerSound = MediaPlayer.create(this, R.raw.cheer);
+
+    public void onCheckboxClicked(CheckBox chkbox) {
+
+        final MediaPlayer cheerSound = MediaPlayer.create(this, R.raw.cheer1);
         cheerSound.start();
+
     }
+    public void onCheckboxUnClicked(CheckBox chkbox) {
+
+        final MediaPlayer awwSound = MediaPlayer.create(this, R.raw.aww1);
+        awwSound.start();
+
+    }
+
 }
+
